7b0959e4f2909b9f8e78a28db24eb5ab15e39606
Dariss Harris <darissharris@gmail.com>
2020-11-05 09:03:14 -0800

    inital upload

---
+++ src/bookmark/bookmark-router.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const { v4: uuid } = require('uuid');
+const logger = require('../logger');
+const bookmarks = require('../bookmarkStore');
+const { router } = require('../app');
+const BookmarksService = require('./bookmarks-service');
+
+const bookmarkRouter = express.Router();
+const bodyParser = express.json();
+
+const serializeBookmark = bookmark => ({
+  id: bookmark.id,
+  title: bookmark.title,
+  url: bookmark.url,
+  description: bookmark.description,
+  rating: Number(bookmark.rating),
+});
+
+bookmarkRouter
+  .route('/bookmarks')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
+    BookmarksService.getAllBookmarks(knexInstance)
+      .then(bookmarks => {
+        res.json(bookmarks.map(serializeBookmark));
+      })
+      .catch(next);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, content } = req.body;
+  
+    if (!title) {
+      logger.error('Title is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+  
+    if (!content) {
+      logger.error('Content is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+  
+    // get an id
+    const id = uuid();
+  
+    const bookmark = {
+      id,
+      title,
+      content
+    };
+  
+    bookmarks.push(bookmark);
+  
+    logger.info(`Bookmark with id ${id} created`);
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json(bookmark);
+  
+  });
+
+bookmarkRouter
+  .route('/bookmarks/:bookmark_id')
+  .get((req, res, next) => {
+    const { bookmark_id } = req.params;
+    BookmarksService.getById(req.app.get('db'), bookmark_id)
+      .then(bookmark => {
+        if (!bookmark) {
+          logger.error(`Bookmark with id ${bookmark_id} not found.`);
+          return res.status(404).json({
+            error: { message: 'Bookmark doesn\'t exist' }
+          });
+        }
+        res.json(serializeBookmark(bookmark));
+      })
+      .catch(next);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Not Found');
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted`);
+
+    res
+      .status(204)
+      .end();
+  });
+
+module.exports = bookmarkRouter;
---
+++ src/bookmarkStore.js
@@ -0,0 +1,14 @@
+const bookmarks = [{
+  id: 1,
+  title: 'Book One',
+  content: 'This is book one'
+},
+{
+  id: 2,
+  title: 'Book Two',
+  content: 'This is book two'
+}];
+
+
+
+module.exports = bookmarks; 
